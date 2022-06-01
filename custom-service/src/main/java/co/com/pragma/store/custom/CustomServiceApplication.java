990cbfc59802e136bbde86b7347d1ce9e5f62694
Jorge <jorgeluisvelasquezv@gmail.com>
2022-06-01 12:53:27 -0500

    correccion puerto de eureka

--- custom-service/src/main/java/co/com/pragma/store/custom/CustomServiceApplication.java
+++ custom-service/src/main/java/co/com/pragma/store/custom/CustomServiceApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomServiceApplication {
 
 	public static void main(String[] args) {
