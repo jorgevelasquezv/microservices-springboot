f90631749077c28188c2146744773d319657cecb
Jorge <jorgeluisvelasquezv@gmail.com>
2022-06-01 10:30:48 -0500

    servidor y clientes funcionando

---
+++ registry-service/src/test/java/co/com/pragma/store/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.pragma.store.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
